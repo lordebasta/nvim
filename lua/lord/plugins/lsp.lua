1dd63057d6645b1cc2d22fd23d91ed12f89da3f4
lordebasta <41568059+lordebasta@users.noreply.github.com>
2024-11-12 17:48:33 +0000

    removed unused language servers

--- lua/lord/plugins/lsp.lua
+++ lua/lord/plugins/lsp.lua
@@ -27,8 +27,8 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
-                "rust_analyzer",
-                "gopls",
+--                "rust_analyzer",
+--                "gopls",
                 "pylsp"
             },
             handlers = {
